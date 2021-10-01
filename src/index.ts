f353aebf2c9fc76887c55cccf0b0c6c0d783322e
joe-afternightfall <joe@afternightfall.com>
2021-09-30 23:01:50 -0500

    adding to exports

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,60 @@
-export {
+export type {
+  ActiveSetInfo,
+  BuiltSets,
+  ParameterType,
+  GripType,
+  GripWidth,
+  TrainingSetType,
+  WorkoutDistance,
+  WorkoutDuration,
+  WorkoutTimer,
+  Set,
+  WorkoutExercise,
+  Segment,
+  Phase,
+  Routine,
+} from './configs/AppInterfaces';
+
+export { gripTypes } from './configs/workout-types/grip-types';
+export { gripWidths } from './configs/workout-types/grip-widths';
+
+export { parameterTypes } from './configs/workout-types/parameter-types';
+export { trainingSetTypes } from './configs/workout-types/training-set-types';
+
+export { MobileWorkoutDAO } from './configs/models/MobileWorkoutDAO';
+export { MobileWorkoutVO } from './configs/models/MobileWorkoutVO';
+
+export { WorkoutCategoryDAO } from './configs/models/WorkoutCategoryDAO';
+export { WorkoutCategoryVO } from './configs/models/WorkoutCategoryVO';
+
+export { RoutineTemplateDAO } from './configs/models/RoutineTemplateDAO';
+export { RoutineTemplateVO } from './configs/models/RoutineTemplateVO';
+
+export { PhaseDAO } from './configs/models/PhaseDAO';
+export { PhaseVO } from './configs/models/PhaseVO';
+
+export { ExerciseVO } from './configs/models/ExerciseVO';
+export { ExerciseDAO } from './configs/models/ExerciseDAO';
+
+export { EquipmentVO } from './configs/models/EquipmentVO';
+export { EquipmentDAO } from './configs/models/EquipmentDAO';
+
+export type {
+  Muscles,
+  SideOfBody,
+  MuscleGroup,
+  BodySection,
+  muscleGroups,
+} from './models/workout-configurations/MuscleGroups';
+
+export type {
   Timer,
   CircuitExercise,
   CircuitExerciseSet,
   WorkoutCircuitProps,
 } from './models/WorkoutDAO';
 
-export { SetTemplate } from './models/CircuitTemplateDAO';
+export type { SetTemplate } from './models/CircuitTemplateDAO';
 export { SetType } from './models/workout-configurations/exercise-type/ExerciseTypeDAO';
 
 export { CircuitTypeDAO } from './models/workout-configurations/circuit-type/CircuitTypeDAO';
