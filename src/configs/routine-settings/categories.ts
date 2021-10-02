bed31c6e8ebb9d51dfbd5d00f6787e6d950875cc
joe-afternightfall <joe@afternightfall.com>
2021-10-02 10:12:04 -0500

    adding more settings

---
+++ src/configs/routine-settings/categories.ts
@@ -0,0 +1,25 @@
+import { WorkoutCategoryVO } from '../models/workout-settings/WorkoutCategoryVO';
+
+export const workoutCategories: WorkoutCategoryVO[] = [
+  {
+    firebaseId: 'c7b03c13-126f-4f9a-afe2-19ecde8a6540',
+    id: '7ce750dc-fc73-43ad-99c3-a60a8b846177',
+    name: 'Back and Biceps',
+    color: '',
+    muscleGroupIds: ['5', '6', '1'],
+  },
+  {
+    firebaseId: 'df4876c5-9aaf-4fa3-b635-66414bc521c4',
+    id: '70f1b717-792c-4986-ba3d-db65c25c329d',
+    name: 'Chest and Triceps',
+    color: '',
+    muscleGroupIds: ['9', '4'],
+  },
+  {
+    firebaseId: '500c9b74-ee52-4878-af0e-1024211d908a',
+    id: '75fff0e4-937f-410b-8080-725b788616c4',
+    name: 'Shoulders and Legs',
+    color: '',
+    muscleGroupIds: ['2', '12', '11'],
+  },
+];
