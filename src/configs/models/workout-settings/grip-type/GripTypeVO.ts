8bcd0f71185a6f59c02f08b33744b1f8cb4a463f
joe-yarbrough <joe@afternightfall.com>
2021-11-02 07:44:03 -0500

    adding active flag

--- src/configs/models/workout-settings/grip-type/GripTypeVO.ts
+++ src/configs/models/workout-settings/grip-type/GripTypeVO.ts
@@ -4,12 +4,12 @@ export class GripTypeVO extends GripTypeDAO {
   firebaseId: string;
 
   constructor(
+    firebaseId: string,
     id: string,
     name: string,
     description: string,
     iconId: string,
-    active: boolean,
-    firebaseId: string
+    active: boolean
   ) {
     super(id, name, description, iconId, active);
     this.firebaseId = firebaseId;
--- src/configs/models/workout-settings/grip-width/GripWidthVO.ts
+++ src/configs/models/workout-settings/grip-width/GripWidthVO.ts
@@ -4,12 +4,12 @@ export class GripWidthVO extends GripWidthDAO {
   firebaseId: string;
 
   constructor(
+    firebaseId: string,
     id: string,
     name: string,
     description: string,
     iconId: string,
-    active: boolean,
-    firebaseId: string
+    active: boolean
   ) {
     super(id, name, description, iconId, active);
     this.firebaseId = firebaseId;
