9a789288033989d69c462becfabc83a96f1f822b
joe-yarbrough <joe@afternightfall.com>
2021-11-09 12:53:50 -0600

    adding id

--- src/configs/constants/training-set-type-ids/training-set-type-ids.ts
+++ src/configs/constants/training-set-type-ids/training-set-type-ids.ts
@@ -1,3 +1,6 @@
 export const SUPER_SET_ID = '3fd3c30c-6c6a-4cfd-8e59-5b500e486374';
 export const STRAIGHT_SET_ID = '40ac0220-8032-4860-81dd-68943d786123';
 export const CIRCUIT_SET_ID = '5b3ba1b6-92a9-489b-a51a-2ac0afdb99ca';
+
+export const PRIMARY_MUSCLE_TARGET_TYPE_ID =
+  '9bc50538-1976-4b18-aace-489f0c5f2c73';
